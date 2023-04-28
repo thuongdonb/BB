c7489ec41a6328feb3c3a7f3a3774701218d4820
Thuong <thuongdonb.2001@gmail.com>
2023-04-28 23:02:59 +0700

    First Commit

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text1: "Chào em!",
+  text2: "Anh có điều này muốn hỏi em nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Em yêu anh có phải không nào ._.",
+  text4: "Nếu em không trả lời mà thoát ra tức là muốn làm vợ anh rùi đó nha ^^",
+  text5: "Em mơ à???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text7: "Lí do em thích anh đi ",
+  text8: "Gửi cho anh <3",
+  text9: "Vì anh đẹp trai hihi",
+  text10: "Anh biết mà ^^ Yêu em 300.000 ^^",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Tối nay anh qua đón em đi chơi nhaa hihi Còn giờ thì chờ gì nữa mà ko inbox cho anh đi nàooo",
   text12: "Okii lunn <3",
 };
 
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "http://fb.com";
+            window.location = "https://www.facebook.com/playboy1nb";
           },
         });
       }
